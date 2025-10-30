52c9c0c0004685a809a25fbc76ff982ac0ab92b0
mossad <mossadmohamed6070@gmail.com>
2025-10-30 13:14:39 +0300

    third commit

--- main.js
+++ main.js
@@ -6,9 +6,17 @@ let brown_frun = document.querySelector(".color_2");
 let trkwas_frun = document.querySelector(".color_3");
 let gray_frun = document.querySelector(".color_4");
 let main_image = document.getElementById("main_image");
+let btn = document.getElementById("btn");
+let cart = document.getElementById("cart");
+let cart_span = document.createElement("span");
+let cart_div = document.querySelector(".cart_div");
 let y = 0;
+cart_span.id="cart_span";
+cart_span.style.cssText = " position: absolute;top: 51px;right: 47px;background-color: rgb(223, 7, 7);padding: 4px;border-radius: 70%;font-size: 6px;";
+btn.onclick = function(){
+    cart_div.appendChild(cart_span);
 
-
+}
 blue_frun.onclick = function(){
     main_image.src="/images/mid-century_Modern_home_chair-removebg-preview-baby_blue color.png";
 }
